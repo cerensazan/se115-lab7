c6325c9e7bec374c6a0c26c68f79f1f1be7e3d97
cerensazan <ceren.sazan12@gmail.com>
2025-12-18 13:32:27 +0300

    Create Students.java

---
+++ Students.java
@@ -0,0 +1,17 @@
+public class Students {
+    public int age;
+    public String name;
+
+    public void printStudentInfo(){
+        System.out.println("Name:"+name);
+        System.out.println("Age:"+age);
+
+    }
+    public Students(int a, String b){
+        age=a;
+        name= b;
+
+    }
+
+
+    }
